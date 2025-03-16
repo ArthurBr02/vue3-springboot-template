421c91cd86bc7815f73afaa75b9fcba607e423f3
Arthur <34396214+ArthurBr02@users.noreply.github.com>
2025-03-16 19:05:53 +0100

    front - initial commit + ajout primevue

---
+++ front/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="mt-3 mb-3 flex justify-center align-center">
+    <RouterView />
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ front/src/main.ts
@@ -0,0 +1,30 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura'
+
+import App from './App.vue'
+import router from './router'
+
+import Button from "primevue/button"
+import Card from "primevue/card";
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura,
+        options: {
+            darkModeSelector: false || 'none',
+        }
+    }
+})
+
+app.component('Button', Button)
+app.component('Card', Card)
+
+app.mount('#app')
