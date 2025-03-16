61308a7b7ec9d8333bcb43778f54db422767e232
Arthur <34396214+ArthurBr02@users.noreply.github.com>
2025-03-16 19:04:29 +0100

    back - initial commit

---
+++ back/src/main/java/fr/arthurbr02/vue3_springboot_template/Vue3SpringbootTemplateApplication.java
@@ -0,0 +1,13 @@
+package fr.arthurbr02.vue3_springboot_template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vue3SpringbootTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Vue3SpringbootTemplateApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/fr/arthurbr02/vue3_springboot_template/Vue3SpringbootTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.arthurbr02.vue3_springboot_template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Vue3SpringbootTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
